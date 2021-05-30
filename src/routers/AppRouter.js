404896b0d56c9ea4ccfa2cc6ac3f9ac30bf15303
Partha <pb25193@gmail.com>
2021-05-30 11:45:30 +0200

    fixed import bug

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Router, Route, Switch} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import DashboardPage from '../components/DashboardPage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
